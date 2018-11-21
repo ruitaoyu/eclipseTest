850a4f4be7b221188781e9072faa1e203fa16f88
ruitaoyu <ruitaoyu@uw.edu>
2018-11-21 11:29:40 -0800

    first

---
+++ src/eclipseTest/Sample.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package eclipseTest;
+
+/**
+ * @author terryyu
+ *
+ */
+public class Sample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
